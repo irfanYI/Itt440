9cab65623b7c14124ce1f1b31ef6d69491a57e3f
irfanYI <32901938+irfanYI@users.noreply.github.com>
2017-10-18 12:22:03 -0700

    serverlua.lua
    server code for lua

---
+++ serverlua.lua
@@ -0,0 +1,14 @@
+socket = require('socket')
+port = 3030
+server = socket.bind('*',port)
+io.write("Listening to port : "..port.."\n")
+sv = server:accept()
+io.write("Connection established\n")
+while true do
+
+  msg = sv:receive()
+  io.write(msg .. "\n")
+  io.write("Respond : ")
+  sv:send(io.read() .. "\n")
+end
+io.read()
